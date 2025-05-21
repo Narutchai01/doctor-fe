fc259fbfcd3d6915577c0b23b9833dc01bc2e43c
Narutchai <koonfluk2015@gmail.com>
2025-05-22 00:16:10 +0700

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['profile.line-scdn.net'],
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  
 };
 
 export default nextConfig;
